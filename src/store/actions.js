efb6965147fc83215319b12580c0f9609dda5260
Daniel Kuroski <daniel.kuroski@gmail.com>
2018-07-07 15:57:05 -0300

    testing store actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,1 +1,11 @@
-export default {}
+export default {
+  INSERT: ({ commit }, payload) => {
+    commit('ADD_INSERT_OPERATION', payload)
+  },
+  UPDATE: ({ commit }, payload) => {
+    commit('ADD_UPDATE_OPERATION', payload)
+  },
+  DELETE: ({ commit }, payload) => {
+    commit('ADD_DELETE_OPERATION', payload)
+  },
+}
--- tests/unit/actions.spec.js
+++ tests/unit/actions.spec.js
@@ -1,5 +1,43 @@
+import actions from '@/store/actions'
+import socketFixture from './fixtures/socketServer'
+
 describe('vuex actions', () => {
-  xit('works', () => {
-    
+  let commit
+
+  beforeEach(() => {
+    commit = jest.fn()
+  })
+
+  it('process INSERT operation', () => {
+    // arranje
+    const expectedPayload = socketFixture.response.INSERT
+
+    // act
+    actions.INSERT({ commit }, expectedPayload)
+
+    // assert
+    expect(commit).toHaveBeenCalledWith('ADD_INSERT_OPERATION', expectedPayload)
+  })
+
+  it('process UPDATE operation', () => {
+    // arranje
+    const expectedPayload = socketFixture.response.UPDATE
+
+    // act
+    actions.UPDATE({ commit }, expectedPayload)
+
+    // assert
+    expect(commit).toHaveBeenCalledWith('ADD_UPDATE_OPERATION', expectedPayload)
+  })
+
+  it('process DELETE operation', () => {
+    // arranje
+    const expectedPayload = socketFixture.response.DELETE
+
+    // act
+    actions.DELETE({ commit }, expectedPayload)
+
+    // assert
+    expect(commit).toHaveBeenCalledWith('ADD_DELETE_OPERATION', expectedPayload)
   })
 })
