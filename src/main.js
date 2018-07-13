be72173e634eab3cd5b51028249ad146c78b928f
Daniel Kuroski <daniel.kuroski@gmail.com>
2018-07-13 00:04:48 -0300

    testing timeline idea

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import VueTimeago from 'vue-timeago'
+import { Timeline } from 'vue2vis'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'vue2vis/dist/vue2vis.css'
 
 Vue.config.productionTip = false
 Vue.use(VueTimeago)
+Vue.component('timeline', Timeline)
 
 new Vue({
   router,
--- src/views/TheOperationsLog.vue
+++ src/views/TheOperationsLog.vue
@@ -1,4 +1,5 @@
 <script>
+import uuidv1 from 'uuid/v1'
 import { mapState } from 'vuex'
 import VOperationsTimeline from '@/components/VOperationsTimeline'
 
@@ -7,16 +8,57 @@ export default {
   components: {
     VOperationsTimeline,
   },
+  data() { 
+    return {
+      groups: [
+        { id: 0, content: 'INSERT' },
+        { id: 1, content: 'UPDATE' },
+        { id: 2, content: 'DELETE' },
+      ],
+      options: {}
+    }
+  },
   computed: {
     ...mapState({
       operations: 'operations'
-    })
-  }
+    }),
+    items() {
+      const ids = {
+        'INSERT': 0,
+        'UPDATE': 1,
+        'DELETE': 2,
+      }
+      return this.operations
+        .map((operation) => ({
+          id: uuidv1(),
+          group: ids[operation.type],
+          type: 'point',
+          start: operation.timestamp,
+          content: `${operation.schema} - ${operation.table}`
+        }))
+    },
+  },
 }
 </script>
 
 <template>
   <div>
+    <timeline
+      ref="timeline"
+      :items="items"
+      :groups="groups"
+      :options="options"
+    ></timeline>
     <VOperationsTimeline :operations="operations" />
   </div>
 </template>
+
+<style>
+.vis-timeline {
+  visibility: visible !important;
+}
+
+.timeline {
+  margin-top: 10px;
+}
+</style>
